90844d25b1fc44aa937c22025810bbea879def92
Giliard31 <113076201+Giliard31@users.noreply.github.com>
2026-09-04 09:13:02 -0300

    Change notification icon and badge to HTTPS URLs
    Updated notification icon and badge URLs to use HTTPS.

--- ws.js
+++ ws.js
@@ -2,8 +2,8 @@ self.addEventListener('push', function(event) {
     const dados = event.data ? event.data.json() : { title: 'Meu Delivery', body: 'Novo pedido ou atualização!' };
     const options = {
         body: dados.body,
-        icon: 'img/icone-192.png',
-        badge: 'img/icone-192.png',
+        icon: 'https://raw.githubusercontent.com/Giliard31/app-delivery/refs/heads/main/img/%C3%ADcone-192.png',
+        badge: 'https://raw.githubusercontent.com/Giliard31/app-delivery/refs/heads/main/img/%C3%ADcone-192.png',
         vibrate: [200, 100, 200]
     };
     event.waitUntil(
